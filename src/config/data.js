737a4a0771d0fbfdde2c73af8eb426853c963b99
Osaze <osareniyeosazee@gmail.com>
2022-08-01 14:41:05 +0100

    updated data

--- src/config/data.js
+++ src/config/data.js
@@ -1,18 +1,17 @@
-export const buttons = [
-  {
-    text: '24 Hours',
-    value: 1,
-  },
-  {
-    text: '30 Days',
-    value: 30,
-  },
-  {
-    text: '3 Months',
-    value: 90,
-  },
-  {
-    text: '1 Year',
-    value: 365,
-  },
+export const buttons = [{
+        text: '24 Hours',
+        value: 1,
+    },
+    {
+        text: '30 Days',
+        value: 30,
+    },
+    {
+        text: '3 Months',
+        value: 80,
+    },
+    {
+        text: '1 Year',
+        value: 365,
+    },
 ]
